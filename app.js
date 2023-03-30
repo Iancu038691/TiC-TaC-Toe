6a6975d0039839a1500ba6c66965c127916eb263
Iancu Stefan-George <iancu.stefan038691@gmail.com>
2023-03-30 23:17:30 +0300

    gameboard function added

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const Gameboard = (() => {
+  gameboard = [];
+  let playerTurn = 0;
+  for (let i = 0; i < 9; i++) {
+    gameboard.push(document.createElement('div'));
+    gameboard[i].classList.add('card');
+    const container = document.querySelector('.container');
+    container.appendChild(gameboard[i]);
+    gameboard[i].onclick = () => {
+      if (playerTurn % 2 == 0 && gameboard[i].textContent === '') { gameboard[i].textContent = 'X'; } else if (playerTurn % 2 !== 0 && gameboard[i].textContent === '') { gameboard[i].textContent = 'O'; }
+      playerTurn += 1;
+    };
+  }
+  return { gameboard, playerTurn };
+})();
+
+const firstPlayer = () => {
+
+};
+
+const secondPlayer = () => {
+
+};
